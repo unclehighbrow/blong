a41c8448eb62f2840dad44c0204df96f2c8d8f1f
Will Carlough <will@carlough.net>
2014-01-11 17:52:51 -0500

    and some bullshit actionscript too

--- Blong/BlongEasing.m
+++ Blong/BlongEasing.m
@@ -16,14 +16,24 @@
     float p = duration*.3;
     float s = p*.25;
     
-    SKAction *ret = [SKAction customActionWithDuration:duration actionBlock:^(SKNode *node, CGFloat timeElapsed) {
+    return [SKAction customActionWithDuration:duration actionBlock:^(SKNode *node, CGFloat timeElapsed) {
         float dicks = powf(2,-10*timeElapsed/duration) * sinf((timeElapsed-s)*(2*M_PI)/p);
         float x = xDelta*dicks + xDelta + start.x;
         float y = yDelta*dicks + yDelta + start.y;
         node.position = CGPointMake(x, y);
     }];
-    
-    return ret;
 }
+
+//public static function easeOut (t:Number, b:Number, c:Number, d:Number):Number {
+//    if ((t/=d) < (1/2.75)) {
+//        return c*(7.5625*t*t) + b;
+//    } else if (t < (2/2.75)) {
+//        return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+//    } else if (t < (2.5/2.75)) {
+//        return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+//    } else {
+//        return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+//    }
+//}
 
 @end
